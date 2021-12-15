b86ac52c268fd530f1314b19aa8456e29acf4f30
SpyingEnvelope <gadi800@gmail.com>
2021-12-15 06:31:50 -0700

    Added bar Chart
    Added Bar Chart

--- mainJS.js
+++ mainJS.js
@@ -1,22 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
-    .then(response => response.json())
-    .then(function (response) {
-        jsonData = JSON.stringify(response)
-        console.log(jsonData)
-    });
-})
 
+let jsonDataString;
 let jsonData;
+let dataSvg;
+let scale;
+
+const w = 1920;
+const h = 180;
+const widthCheck = window.innerWidth;
+const heightCheck = window.innerHeight;
+
+console.log(widthCheck + ' ' + heightCheck)
 
 fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
     .then(response => response.json())
     .then(function (response) {
         console.log('Date ' + response.data[2][0] + ' GDP ' + response.data[2][1]);
-        console.log(response.data);
-        jsonData = JSON.stringify(response);
-        $('#valueme').html(jsonData);
+        jsonDataString = JSON.stringify(response);
+        jsonData = response;
+        console.log(jsonData);
+        addSvg();
+        // $('#valueme').html(jsonDataString);
 });
+
+const addSvg = () => {
+    dataSvg = d3.select('#chart-div')
+                .append('svg')
+                .attr('width', w)
+                .attr('height', h);
+    addRect();
+}
+
+const addRect = () => {
+    dataSvg.selectAll('rect')
+           .data(jsonData.data)
+           .enter()
+           .append('rect')
+           .attr('x', (d, i) => i * 6)
+           .attr('y', 0)
+           .attr('width', 5)
+           .attr('height', 180)
+}
 
 class MainComponent extends React.Component {
     constructor(props) {
