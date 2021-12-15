ac0f2b5eedb99539ae870b2a96b4c5f407adcad7
SpyingEnvelope <gadi800@gmail.com>
2021-12-15 07:13:57 -0700

    Added xScale

--- mainJS.js
+++ mainJS.js
@@ -3,9 +3,11 @@ let jsonDataString;
 let jsonData;
 let dataSvg;
 let heightScale;
+let xScale;
 
 const w = 1920;
 const h = 500;
+const padding = 40;
 const widthCheck = window.innerWidth;
 const heightCheck = window.innerHeight;
 
@@ -34,9 +36,18 @@ const addScale = () => {
     const minY = d3.min(jsonData.data, (d) => d[1])
     const maxY = d3.max(jsonData.data, (d) => d[1])
 
+    const minX = d3.min(jsonData.data, (d) => d[0])
+    const maxX = d3.max(jsonData.data, (d) => d[0])
+
     heightScale = d3.scaleLinear()
                     .domain([minY, maxY])
-                    .range([10, 500]);
+                    .range([padding, h - padding]);
+    
+    xScale = d3.scaleLinear()
+               .domain([minX, maxX])
+               .range([padding, w - padding]);
+
+    console.log(d3.max(jsonData.data, (d) => d[0]))
     addRect();
 }
 
@@ -45,10 +56,21 @@ const addRect = () => {
            .data(jsonData.data)
            .enter()
            .append('rect')
+           .attr('class', 'bar')
            .attr('x', (d, i) => i * 6)
-           .attr('y', 0)
+           .attr('y', (d, i) => h - heightScale(d[1]))
            .attr('width', 5)
-           .attr('height', (d) => heightScale(d[1]))
+           .attr('height', (d) => heightScale(d[1]));
+
+    addAxisBottom();
+}
+
+const addAxisBottom = () => {
+    const xAxis = d3.axisBottom(xScale);
+    
+    dataSvg.append('g')
+           .attr("transform", "translate(0," + (h - padding) + ")")
+           .call(xAxis)
 }
 
 class MainComponent extends React.Component {
